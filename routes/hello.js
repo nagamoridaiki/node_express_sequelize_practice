3b5eb1363496caabbb4e52b8a275c719d1e9f643
nagamoridaiki <no.swea1017@gmail.com>
2021-01-30 18:09:14 +0900

    tsudasyono_chapter5_mysql

--- routes/hello.js
+++ routes/hello.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const http = require('https'); 
-const parseString = require('xml2js').parseString;
 
-router.get('/', (req, res, next) => {
-  var opt = {
-    host: 'news.google.com',
-    port: 443,
-    path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
-  };
-  http.get(opt, (res2) => {
-    var body = '';
-    res2.on('data', (data) => {
-      body += data;
-    });
-    res2.on('end', () => {
-      parseString(body.trim(), (err, result) => {
-        console.log(result);
-        var data = {
-          title: 'Google News',
-          content: result.rss.channel[0].item
-        };
-        res.render('hello', data);
-      });
-    })
+const mysql = require('mysql')
+
+// データベースオブジェクトの取得
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'testdb'
   });
+
+// GETアクセスの処理
+router.get('/',(req, res, next) => {
+  db.query('select * from mydata', function (err, rows) {
+    if (!err) {
+      console.log(err);
+      var data = {
+        title: 'Hello!',
+        content: rows // 取得したレコードデータ
+      };
+      res.render('hello', data);
+    }  
+  });
+  
 });
 
 module.exports = router;
