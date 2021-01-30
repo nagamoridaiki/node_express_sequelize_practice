a99b7503a5949386661a229ed541e88d3ceb4cdb
nagamoridaiki <no.swea1017@gmail.com>
2021-01-30 15:24:17 +0900

    tsudasyono_chapter2

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 const express = require('express')
 const ejs = require('ejs')
 const mysql = require('mysql')
 const app = express()
 const Sequelize = require('sequelize');
- 
-app.set('ejs', ejs.renderFile)
- 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'testdb'
-  });
- 
-app.get('/', (req, res) => {
- 
-    con.query('select * from title', function (err, results, fields) {
-        if (err) throw err
-        res.render('index.ejs', { content: results })
-    });
- 
-})
- 
-app.listen(3000, () => {
-    console.log('server start')
-})
+
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const other_page = fs.readFileSync('./other.ejs', 'utf8'); //★追加
+const style_css = fs.readFileSync('./style.css', 'utf8');
+
+var server = http.createServer(getFromClient);
+
+server.listen(3000);
+console.log('Server start!');
+
+// ここまでメインプログラム==========
+
+// createServerの処理
+function getFromClient(request, response) {
+
+  var url_parts = url.parse(request.url);
+  switch (url_parts.pathname) {
+
+    case '/':
+      var content = ejs.render(index_page, {
+        title: "Index",
+        content: "これはIndexページです。",
+      });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(content);
+      response.end();
+      break;
+
+    case '/other': //★追加
+      var content = ejs.render(other_page, {
+        title: "Other",
+        content: "これは新しく用意したページです。",
+      });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(content);
+      response.end();
+      break;
+
+    default:
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.end('no page...');
+      break;
+  }
+}
